9d0cf641300b96b129f7034c1feaf88b880eed2f
Romman Sabbir <rommansabbir@gmail.com>
2021-11-13 20:34:37 +0600

    -> FailureHandler added including DI

--- app/src/main/java/com/rommansabbir/androidcleanarchmvvmdi/base/exception/AppFailure.kt
+++ app/src/main/java/com/rommansabbir/androidcleanarchmvvmdi/base/exception/AppFailure.kt
@@ -4,6 +4,6 @@ package com.rommansabbir.androidcleanarchmvvmdi.base.exception
  * To manage app level failures
  */
 sealed class AppFailure : Failure() {
-    class GenericError(val message: String = "Just a random error")
+    class GenericError(val message: String = "Just a random error") : AppFailure()
 }
 
