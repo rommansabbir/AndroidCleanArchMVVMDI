e572ae97ef1cbfc6a14706c589232e918898157b
Romman Sabbir <rommansabbir@gmail.com>
2021-11-13 19:24:26 +0600

    -> Code refactored

--- app/src/main/java/com/rommansabbir/androidcleanarchmvvmdi/base/di/application/MyApplication.kt
+++ app/src/main/java/com/rommansabbir/androidcleanarchmvvmdi/base/di/application/MyApplication.kt
@@ -1,4 +1,4 @@
-package com.rommansabbir.androidcleanarchmvvmdi
+package com.rommansabbir.androidcleanarchmvvmdi.base.di.application
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
