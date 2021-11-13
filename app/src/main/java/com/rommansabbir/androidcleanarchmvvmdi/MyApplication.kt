9dfdf36298496bc90cb61a595921d75971de7113
Romman Sabbir <rommansabbir@gmail.com>
2021-11-13 19:04:06 +0600

    -> MyApplication added with DI

---
+++ app/src/main/java/com/rommansabbir/androidcleanarchmvvmdi/MyApplication.kt
@@ -0,0 +1,9 @@
+package com.rommansabbir.androidcleanarchmvvmdi
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MyApplication : Application() {
+
+}
