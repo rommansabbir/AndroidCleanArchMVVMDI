f1043d10b851040a2ca6bb50658b92a56f6704fb
Romman Sabbir <romman.sabbir@amiprobashi.com>
2021-11-15 11:09:10 +0600

    AuthActivity -> - Added `Factory` to start activity instance

--- app/src/main/java/com/rommansabbir/androidcleanarchmvvmdi/features/auth/AuthActivity.kt
+++ app/src/main/java/com/rommansabbir/androidcleanarchmvvmdi/features/auth/AuthActivity.kt
@@ -1,5 +1,7 @@
 package com.rommansabbir.androidcleanarchmvvmdi.features.auth
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.navigation.fragment.NavHostFragment
@@ -10,8 +12,24 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class AuthActivity : BaseActivity<ActivityAuthBinding>() {
+    object Factory {
+        fun startActivity(
+            context: Activity,
+            shouldFinish: Boolean,
+            payload: Bundle?
+        ) {
+            context.startActivity(
+                Intent(context, AuthActivity::class.java).apply {
+                    payload?.let { putExtra("payload", it) }
+                })
+            if (shouldFinish) {
+                context.finish()
+            }
+        }
+    }
+
     internal val vm: AuthViewModel by viewModels()
-    
+
     override val layoutRes: Int
         get() = R.layout.activity_auth
 
