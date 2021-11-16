92ebb5eba7bb67b0048253f0eb78f371458416a3
Romman Sabbir <romman.sabbir@amiprobashi.com>
2021-11-16 13:47:49 +0600

    RepositoryModule -> - WeatherRepository added for DI

--- app/src/main/java/com/rommansabbir/androidcleanarchmvvmdi/base/di/modules/RepositoryModule.kt
+++ app/src/main/java/com/rommansabbir/androidcleanarchmvvmdi/base/di/modules/RepositoryModule.kt
@@ -4,6 +4,9 @@ import com.rommansabbir.androidcleanarchmvvmdi.base.service.ApiClient
 import com.rommansabbir.androidcleanarchmvvmdi.data.remote.auth.api.AuthAPIService
 import com.rommansabbir.androidcleanarchmvvmdi.data.remote.auth.repository.AuthRepository
 import com.rommansabbir.androidcleanarchmvvmdi.data.remote.auth.repository.AuthRepositoryImpl
+import com.rommansabbir.androidcleanarchmvvmdi.data.remote.weather.WeatherRepository
+import com.rommansabbir.androidcleanarchmvvmdi.data.remote.weather.WeatherRepositoryImpl
+import com.rommansabbir.androidcleanarchmvvmdi.data.remote.weather.api.WeatherAPIService
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -15,4 +18,11 @@ object RepositoryModule {
     @Provides
     fun provideAuthRepository(apiClient: ApiClient, service: AuthAPIService): AuthRepository =
         AuthRepositoryImpl(apiClient, service)
+
+    @Provides
+    fun provideWeatherRepository(
+        apiClient: ApiClient,
+        service: WeatherAPIService
+    ): WeatherRepository =
+        WeatherRepositoryImpl(apiClient, service)
 }
