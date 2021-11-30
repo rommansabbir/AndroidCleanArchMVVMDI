c533e5ef9fad28f98e4606b32f13cf20b5caa65f
Romman Sabbir <romman.sabbir@amiprobashi.com>
2021-11-30 12:01:04 +0600

    Documentation -> - Added README.md

---
+++ README.md
@@ -0,0 +1,14 @@
+# Android Clean Architecture Kick Start project with MVVM, Coroutine, DI, Caching, Netwokring
+
+
+Stacks:
+* Clean Architecture
+* MVVVM
+* Local Caching (StoreX)
+* Networking (Retrofit, OkHttp)
+* Data Binding
+* UseCase
+* Dependency Injection (Hilt)
+* Navigator (Navigation)
+* Failures
+* Extension Functions
--- app/src/main/java/com/rommansabbir/androidcleanarchmvvmdi/base/navigator/home/HomeNavigator.kt
+++ app/src/main/java/com/rommansabbir/androidcleanarchmvvmdi/base/navigator/home/HomeNavigator.kt
@@ -4,6 +4,10 @@ import android.content.Context
 import com.rommansabbir.androidcleanarchmvvmdi.base.navigator.Navigator
 import dagger.hilt.android.qualifiers.ActivityContext
 
+/**
+ * This is the feature specific navigator.
+ * Write down all the possible navigation flow here.
+ */
 interface HomeNavigator : Navigator {
     fun navigateToHome(@ActivityContext context: Context)
 }
